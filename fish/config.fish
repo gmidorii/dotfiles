bc5e9a74fca7b4b7ca6340f57db416cfcf61fa6c
tagasouichiro <stitow14@gmail.com>
2019-01-14 16:21:15 +0900

    Add fish file

---
+++ fish/config.fish
@@ -0,0 +1,11 @@
+if test $TERM != "screen"
+ #tmux attach
+end
+
+source ~/dotfiles/fish/alias.fish
+source ~/dotfiles/fish/path.fish
+
+function fish_user_key_bindings
+	bind \cr 'peco_select_history (commandline -b)'
+	bind \c] peco_select_ghq_repository
+end
